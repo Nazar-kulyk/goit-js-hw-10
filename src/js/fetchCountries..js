079adff8f06f42a99ad1aef9d8fbdf5727e5b269
Nazar-Kulyk <nazarkulyk1993@gmail.com>
2023-03-20 19:23:54 +0100

    fixt problems

--- src/js/fetchCountries..js
+++ src/js/fetchCountries..js
@@ -1,10 +1,10 @@
 export function fetchCountries(name) {
   return fetch(
     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
+  ).then(responce => {
+    if (!responce.ok) {
+      throw new Error(responce.status);
     }
-    return response.json();
+    return responce.json();
   });
 }
